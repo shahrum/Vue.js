135e8189a50e956c57d82769d58995a2f0a6989b
shha268@gmail.com <shha268@gmail.com>
2018-02-18 13:35:40 +1100

    Adding some Html&Css
    Use bulma.io for better understading

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -1,7 +1,55 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item">MyCompany</a>
+        </div>
+
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="nav-right nav-menu">
+
+          <router-link to="/" class="nav-item r-item">Home</router-link>
+          <router-link to="faq" class="nav-item r-item">Features</router-link>
+          <router-link to="faq" class="nav-item r-item">About</router-link>
+          <router-link to="faq" class="nav-item r-item">FAQ</router-link>
+
+          <div class="nav-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+
+    <router-view></router-view>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>And this right here is a spiffy footer, where you can put stuff.</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -16,12 +64,32 @@ export default {
 @import '../node_modules/bulma/bulma.sass'
 @import 'mediaquery'
 
-#app 
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
+.nav
+  background-color: #383838
+  a:hover
+    color:#fff
 
+.nav-left a
+  font-weight: bold
+  color: #fff
+
+a.r-item
+  color: #c1c1c1
+  padding: 0.5rem 1.75rem
+  +mobile
+    color: #FFF
+    &:hover
+      background-color: #F00
+
+.nav-left,.nav-right
+  float: left
+
+.nav-right.nav-menu .nav-item
+  float: left
+
+.clear
+  clear: both !important
+
+.nav.has-shadow
+  padding: 10px
 </style>
